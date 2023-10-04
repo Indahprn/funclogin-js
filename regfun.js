69f2e6fffa86f14c44676ab75d2c3d2bb567db08
Indah Putri Rahmi Nurilah <131329672+Indahprn@users.noreply.github.com>
2023-10-04 14:55:48 +0700

    Add files via upload

---
+++ regfun.js
@@ -0,0 +1,9 @@
+function login (usn, pass) {
+    console.log("---------------------------")
+    console.log("           login           ")
+    console.log("---------------------------")
+    console.log("username: " + usn)
+    console.log("password: " + pass)
+    console.log("---------------------------")
+}
+login("indahprn", "010906")
